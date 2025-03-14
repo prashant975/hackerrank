bac7fa833f11ecc24b8503133ab57cef9a854642
prashant975 <rashantp10@gmail.com>
2025-03-14 10:07:07 +0530

    Create Loops_N Multiples.c

---
+++ Loops_N Multiples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int x;
+    scanf("%d",&x);
+    for(int i=1;i<=n;++i){
+        
+            printf("%d ",x*i);
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
