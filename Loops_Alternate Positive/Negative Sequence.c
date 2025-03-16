5c7c49f260b09002fd7b0b4fdecc2f459214ff9b
prashant975 <rashantp10@gmail.com>
2025-03-16 23:32:56 +0530

    Create Negative Sequence.c

---
+++ Loops_Alternate Positive/Negative Sequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d ",&n);
+    for(int i=1; i<=n;++i){
+        int x=-i;
+        if(i%2==0)
+            
+        printf("%d ",x);
+        else{
+        printf("%d ",i);
+        }
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
