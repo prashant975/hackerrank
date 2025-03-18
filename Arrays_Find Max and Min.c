2f1f7d5d085cee77f4fa020bf515ebd337471455
prashant975 <rashantp10@gmail.com>
2025-03-18 23:25:49 +0530

    Create Arrays_Find Max and Min.c

---
+++ Arrays_Find Max and Min.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    
+    for(int i=0;i<=n-1;++i){
+        scanf("%d",&a[i]);
+        
+    }
+    int max =a[0];
+    int min =a[0];
+    for(int i=1;i<n;++i){
+        if(a[i]>max){
+            max=a[i];
+        }
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    printf("%d %d",max ,min);
+    
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
