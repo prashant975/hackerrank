ebd1a4a23c12be42621b233a418f8556f1d08b5d
prashant975 <rashantp10@gmail.com>
2025-03-23 10:08:53 +0530

    Create Bitwise_Toggle i-th bit.c

---
+++ Bitwise_Toggle i-th bit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int i;
+    scanf("%d",&i);
+    n=n^(1<<i);
+    printf("%d",n);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
