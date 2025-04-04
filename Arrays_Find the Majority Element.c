c46bed3f22793f646b7be1ed184de1a8ce1dae14
prashant975 <rashantp10@gmail.com>
2025-04-04 09:51:54 +0530

    Create Arrays_Find the Majority Element.c

---
+++ Arrays_Find the Majority Element.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int a[n]; 
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int freq[2000001]={0};
+    for (int i=0;i<n;i++){
+
+            freq[a[i]+1000000]++;
+    }
+    for (int i=0;i<n;i++){
+        if(freq[a[i]+1000000]>n/2){
+            printf("%d",a[i]);
+            return 0;
+        }
+    }
+    printf("No Majority Element");
+    return 0;
+}
